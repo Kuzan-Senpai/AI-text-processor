9a520b8bd9c368a28f90be8b72e06889905a9b8d
Your Name <oghenetegafrancis@gmail.com>
2025-02-20 12:41:16 +0100

    token

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,11 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         {includeMetaTag && (
-          <><meta httpEquiv="origin-trial" content="AiTFG2cRssYEcIfJun+0k9D4rxw6HaD4FJCN/UP10Cfv7Z2ORpEBFJaHQjDKRRo1aaTYibc7LP+KydrB9nXr7wkAAAB8eyJvcmlnaW4iOiJodHRwczovL2FpLXRleHQtcHJvY2Vzc29yLW5pbmUudmVyY2VsLmFwcDo0NDMiLCJmZWF0dXJlIjoiVHJhbnNsYXRpb25BUEkiLCJleHBpcnkiOjE3NTMxNDI0MDAsImlzU3ViZG9tYWluIjp0cnVlfQ==" />
+          <>
+          <meta httpEquiv="origin-trial" content="AiTFG2cRssYEcIfJun+0k9D4rxw6HaD4FJCN/UP10Cfv7Z2ORpEBFJaHQjDKRRo1aaTYibc7LP+KydrB9nXr7wkAAAB8eyJvcmlnaW4iOiJodHRwczovL2FpLXRleHQtcHJvY2Vzc29yLW5pbmUudmVyY2VsLmFwcDo0NDMiLCJmZWF0dXJlIjoiVHJhbnNsYXRpb25BUEkiLCJleHBpcnkiOjE3NTMxNDI0MDAsImlzU3ViZG9tYWluIjp0cnVlfQ==" />
           <meta httpEquiv="origin-trial" content="Aji+Z/d5aYG3E6jO3jOaHOXFqevlZWkR5gSoUb1AP9OAeZMPSV0GZHkiB+z5i2RVpivqF+ZaeGiEVE3d7GZViwEAAACCeyJvcmlnaW4iOiJodHRwczovL2FpLXRleHQtcHJvY2Vzc29yLW5pbmUudmVyY2VsLmFwcDo0NDMiLCJmZWF0dXJlIjoiTGFuZ3VhZ2VEZXRlY3Rpb25BUEkiLCJleHBpcnkiOjE3NDk1OTk5OTksImlzU3ViZG9tYWluIjp0cnVlfQ==" />
           <meta httpEquiv="origin-trial" content="AvdyQMFdavMX6YK/H+VTbH1HZTG/hrZeKsWpW0vCDKBtHxGGwuGXrRFRaGrZLJBMfN+SlK2vmTRoEpxeT+CSTQIAAACAeyJvcmlnaW4iOiJodHRwczovL2FpLXRleHQtcHJvY2Vzc29yLW5pbmUudmVyY2VsLmFwcDo0NDMiLCJmZWF0dXJlIjoiQUlTdW1tYXJpemF0aW9uQVBJIiwiZXhwaXJ5IjoxNzUzMTQyNDAwLCJpc1N1YmRvbWFpbiI6dHJ1ZX0=" />
+
           </>
         )}
       </head>
